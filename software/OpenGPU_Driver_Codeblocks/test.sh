3a61ea6263a5e4ad648886c594dd16a00a570a1c
root <root@localhost.localdomain>
2016-11-24 12:30:48 -0200

    Hardware initial testing with DE1 SoC -- basic LED test and software integration in modified softpipe

---
+++ software/OpenGPU_Driver_Codeblocks/test.sh
@@ -0,0 +1,6 @@
+export LD_LIBRARY_PATH=$PWD/../mesa/lib
+export LIBGL_DRIVERS_PATH=$PWD/../mesa/lib
+export LIBGL_DRIVERS_PATH=$PWD/../mesa/lib/gallium
+export LIBGL_ALWAYS_SOFTWARE=1
+export GALLIUM_PRINT_OPTIONS=1
+
